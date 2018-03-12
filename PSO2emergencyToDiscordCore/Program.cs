dc9cba0c27b0abbc17ae99d7ee0d1b5dd378c521
高速人 <kousokujin@outlook.jp>
2018-03-12 18:17:08 +0900

    プロジェクト ファイルを追加します。

---
+++ PSO2emergencyToDiscordCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PSO2emergencyToDiscordCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
