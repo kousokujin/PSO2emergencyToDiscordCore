1d2f14155867e308aff58ed43ef8311d54f9f9d0
kousokujin <kousokujin@outlook.jp>
2018-03-14 02:28:43 +0900

    コミットがうまくいってなかった

--- PSO2emergencyToDiscordCore/Program.cs
+++ PSO2emergencyToDiscordCore/Program.cs
@@ -6,22 +6,52 @@ namespace PSO2emergencyToDiscordCore
 {
     class Program
     {
+        static botController bot;
+        static EventAdmin admin;
+
         static void Main(string[] args)
         {
             Console.OutputEncoding = Console.OutputEncoding;
 
+            printTitle();
+
             HttpClient hc = new HttpClient();
             string geturl = "https://akakitune87.net/api/v4/pso2emergency";
-            string discordUrl = "";
+            string discordUrl = "https://discordapp.com/api/webhooks/348322898089345032/yzcePYWS5nxgRIMNTKKgFPxgOTnEQY9aPY3FXyj5VR_hnO_aivZciwAjgO0EORUUBIPF";
 
             aki_luaEventGetter getPSO2 = new aki_luaEventGetter(geturl, hc);
-            EventAdmin admin = new EventAdmin(getPSO2);
-            DiscordService service = new DiscordService(discordUrl, hc);
-            botController bot = new botController(service, admin);
+            admin = new EventAdmin(getPSO2);
+            AbstractService service = new DiscordService(discordUrl, hc);
+            bot = new botController(service, admin);
 
-            bot.ToServicePOST("テスト投稿\n改行テスト");
+            bool end = false;
+            do
+            {
+                string command = System.Console.ReadLine();
+                commandRun(command);
+                end = (command == "stop");
 
-            System.Console.ReadKey();
+            } while (end == false);
+        }
+
+        static void printTitle()
+        {
+            System.Console.WriteLine("-------------------------------");
+            System.Console.WriteLine("PSO2emergencyToDiscordCore");
+            System.Console.WriteLine("version 1.0.0.0");
+            System.Console.WriteLine("Copyright (c) 2017 Kousokujin.");
+            System.Console.WriteLine("Released under the MIT license.");
+            System.Console.WriteLine("-------------------------------");
+        }
+
+        static void commandRun(string command)
+        {
+            string[] commandData = command.Split(' ');
+
+            if(commandData[0] == "post")
+            {
+                bot.ToServicePOST(commandData[1]);
+            }
         }
     }
 }
