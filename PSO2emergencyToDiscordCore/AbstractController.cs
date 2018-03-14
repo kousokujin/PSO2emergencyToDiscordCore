46b25b3fb5beb8c0846bfe663aee840df21f103b
kousokujin <kousokujin@outlook.jp>
2018-03-14 12:59:02 +0900

    コントローラークラス実装

---
+++ PSO2emergencyToDiscordCore/AbstractController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSO2emergencyToDiscordCore
+{
+    abstract class AbstractController
+    {
+
+        List<string> commandList;
+        protected bool end;   //終了フラグ
+
+        public AbstractController(List<string> cmdList)
+        {
+            end = false;
+            commandList = cmdList;
+
+        }
+
+        public bool checkCommand(string command)
+        {
+            foreach(string c in commandList)
+            {
+                if(c == command)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public string[] Separate(string input)
+        {
+            return input.Split(' ');
+        }
+
+        public void Process(string str)
+        {
+            string[] commandArr = Separate(str);
+            bool check = checkCommand(commandArr[0]);
+
+            if(check == false)
+            {
+                System.Console.WriteLine("コマンドが見つかりません。");
+                return;
+            }
+
+            string[] args = new string[commandArr.Length-1];
+            int i = 0;
+            foreach(string st in commandArr)
+            {
+                if (i != 0)
+                {
+                    args[i - 1] = st;
+                }
+
+                i++;
+            }
+            commandProcess(commandArr[0], args);
+            
+        }
+
+        public void loop()
+        {
+            do
+            {
+                string commandInput = System.Console.ReadLine();
+                Process(commandInput);
+
+            } while (end == false);
+        }
+
+        public abstract void commandProcess(string command, string[] args); //コマンドの処理
+    }
+}
---
+++ PSO2emergencyToDiscordCore/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace PSO2emergencyToDiscordCore
+{
+    class Controller:AbstractController
+    {
+        HttpClient hc;
+        AbstractEventGetter emgGetter;
+        EventAdmin admin;
+        botController bot;
+        AbstractService service;
+
+        string discordurl;
+        string HttpGetUrl;
+
+        Controller(List<string> lst) : base(lst)
+        {
+            init();
+            loop();
+        }
+
+        private void init()
+        {
+            HttpGetUrl = "https://akakitune87.net/api/v4/pso2emergency";
+            discordurl = "https://discordapp.com/api/webhooks/348322898089345032/yzcePYWS5nxgRIMNTKKgFPxgOTnEQY9aPY3FXyj5VR_hnO_aivZciwAjgO0EORUUBIPF";
+
+            hc = new HttpClient();
+            emgGetter = new aki_luaEventGetter(HttpGetUrl, hc);
+            service = new DiscordService(discordurl, hc);
+            admin = new EventAdmin(emgGetter);
+            bot = new botController(service, admin);
+
+        }
+
+        public override void commandProcess(string command, string[] args)
+        {
+            if(command == "stop")
+            {
+                end = true;
+            }
+        }
+
+    }
+}
